87ea5f93bf5697f907c2cb93e0d5bb7e696629c0
Andreas-Oian <88599321+Andreas-Oian@users.noreply.github.com>
2022-10-07 20:57:00 +0200

    Update sensors.cpp

--- Boat/sensors.cpp
+++ Boat/sensors.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <list> 
 
-float convertTempDataToCelsius(tempSensorOutput) {
+float convertTempDataToCelsius(int integerUOut, int R1,) {
 	float temp;
 		
 	// convert raw temperature sensor output data to celsius
-
+	
+	// converting integerUOut to Volt
+	float UOut = integerUOut * 5 / 1023;
+	
+	// calculating resistance (R2) of NTC thermistor: sheet ELS101.1
+	float R2 = R1 / ((5.0 / UOut) - 1);
+	
+	// converting R2 to celsius, thermistor calibration required of form: T = a * b^R
+	float a = 1.0;
+	float b = 1.0;
+	
+	temp = a * pow(b, R2);
 	return temp;
 }
 
