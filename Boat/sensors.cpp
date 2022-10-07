0e079d64087f201b2a1d5dd469c4b77b350a771d
Tmpecho <82368148+Tmpecho@users.noreply.github.com>
2022-10-07 16:29:39 +0200

    Update sensors.cpp

--- Boat/sensors.cpp
+++ Boat/sensors.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
-void test() {
-	std::cout << "hello world" << std::endl
+float convertTempDataToCelsius(tempSensorOutput) {
+	float temp;
+		
+	// convert raw temperature sensor output data to celsius
+
+	return temp;
+}
+
+float convertPressureDataToPascal(pressureSensorOutput) {
+	float pressure;
+
+	// convert raw pressure sensor output data to pascals
+
+	return pressure;
+}
+
+float convertSalineDataToPercent(salinitySensorOutput) {
+	float salinity;
+
+	// convert raw salinity sensor output data to g/kg
+
+	return salinity;
 }
