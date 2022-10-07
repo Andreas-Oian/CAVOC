55e3cb0d450a3046d16cfead5c812b4b5974d03c
Tmpecho <82368148+Tmpecho@users.noreply.github.com>
2022-10-07 16:33:39 +0200

    Update main.ino

--- Boat/main.ino
+++ Boat/main.ino
@@ -1,19 +1,25 @@
 #import <sensors.cpp>
 
+int updateFrequency = 2000  // ms, modifiable
+
 void setup() {
 	Serial.begin(9600);
 }
 
 void loop() {
 	// return getSensorData() values to a list of current data to send to land
-	delay(2000);
+	delay(updateFrequency);  // time between sensor update
 }
 
 float getSensorData() {
+	// init sensor vars
 	float tempSensorOutput;
 	float pressureSensorOutput;
 	float salinitySensorOutput;
 	
+	// here we are going to assign the vars the raw input data from the sensors
+	
+	// convert raw data
 	float temp = convertTempDataToCelsius(tempSensorOutput);
 	float pressure = convertPressureDataToPascal(pressureSensorOutput);
 	float salinity = convertSalineDataToPercent(salinitySensorOutput);
