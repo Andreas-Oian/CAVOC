b25a612265b3e87e293b3e41e6b576d50f6a05ff
Tmpecho <82368148+Tmpecho@users.noreply.github.com>
2022-10-07 16:29:22 +0200

    Update main.ino

--- Boat/main.ino
+++ Boat/main.ino
@@ -1,7 +1,22 @@
-void setup() {
+#import <sensors.cpp>
 
+void setup() {
+	Serial.begin(9600);
 }
 
 void loop() {
+	// return getSensorData() values to a list of current data to send to land
+	delay(2000);
+}
 
+float getSensorData() {
+	float tempSensorOutput;
+	float pressureSensorOutput;
+	float salinitySensorOutput;
+	
+	float temp = convertTempDataToCelsius(tempSensorOutput);
+	float pressure = convertPressureDataToPascal(pressureSensorOutput);
+	float salinity = convertSalineDataToPercent(salinitySensorOutput);
+
+	return temp, pressure, salinity;
 }
